0e018c52b093eac38d898fd1fec81258df1bd2e6
Rachel <rachel.li4125@gmail.com>
2024-11-19 18:32:56 -0500

    fixed navbar

--- adc/src/components/data.ts
+++ adc/src/components/data.ts
@@ -1,16 +1,16 @@
-const lectSlides = [
-    {id: 1, slideName: "Intro", url: "https://docs.google.com/presentation/d/1VP9mrEZJZ9ALk2dBwadcGkWBg5twjUbM6VFZ7Fn3Vkk/edit?usp=sharing" },
-    {id: 2, slideName: "Git,HTML,CSS", url: "https://docs.google.com/presentation/d/1wO047LhrT73QIcC5WzFhzGOtYhxap3aPq-nYbjmOSKk/edit?usp=sharing"},
-    {id: 3, slideName: "JavaScript", url: "https://docs.google.com/presentation/d/1RWvO8TQ_ueJyBdSHfZ6oNvq9E6J3rYovItbX_Q-r-44/edit?usp=sharing"},
-    {id: 4, slideName: "TypeScript", url: "https://docs.google.com/presentation/d/14ooPTPyM4QZPWMBq2sg4NypMQZAHQ4rY5n6CUn6l7zI/edit?usp=sharing"},
-    {id: 5, slideName: "React", url: "https://docs.google.com/presentation/d/1YzEswdGs5zqZMaK8zPCaJl8PiiFFOYDnz2QVHLDAxak/edit?usp=sharing"},
-    {id: 6, slideName: "State in React", url: "https://docs.google.com/presentation/d/1GTiIFoT1EDLZ0Y9SC6G1f9c1-YZoMM-NMLOC8-0_-lI/edit?usp=sharing"}
-]
+// const lectSlides = [
+//     {id: 1, slideName: "Intro", url: "https://docs.google.com/presentation/d/1VP9mrEZJZ9ALk2dBwadcGkWBg5twjUbM6VFZ7Fn3Vkk/edit?usp=sharing" },
+//     {id: 2, slideName: "Git,HTML,CSS", url: "https://docs.google.com/presentation/d/1wO047LhrT73QIcC5WzFhzGOtYhxap3aPq-nYbjmOSKk/edit?usp=sharing"},
+//     {id: 3, slideName: "JavaScript", url: "https://docs.google.com/presentation/d/1RWvO8TQ_ueJyBdSHfZ6oNvq9E6J3rYovItbX_Q-r-44/edit?usp=sharing"},
+//     {id: 4, slideName: "TypeScript", url: "https://docs.google.com/presentation/d/14ooPTPyM4QZPWMBq2sg4NypMQZAHQ4rY5n6CUn6l7zI/edit?usp=sharing"},
+//     {id: 5, slideName: "React", url: "https://docs.google.com/presentation/d/1YzEswdGs5zqZMaK8zPCaJl8PiiFFOYDnz2QVHLDAxak/edit?usp=sharing"},
+//     {id: 6, slideName: "State in React", url: "https://docs.google.com/presentation/d/1GTiIFoT1EDLZ0Y9SC6G1f9c1-YZoMM-NMLOC8-0_-lI/edit?usp=sharing"}
+// ]
 
-const hwAssignments = [
-    {id: 1, hwName: "HW1", url: "https://forms.gle/MQejHVuzuVzj3uGE7"},
-    {id: 2, hwName: "HW2", url: "https://forms.gle/6XVNWAyVc5ADfZxz9"},
-    {id: 3, hwName: "HW3", url: "https://forms.gle/fhbZwTntgESBEY6j8"},
-    {id: 4, hwName: "HW4", url: "https://forms.gle/2bRc93qwD8nwJafu5"},
-    {id: 5, hwName: "Hw5", url: "https://forms.gle/65u4Er1kCaEMJioa7"}
-]
+// const hwAssignments = [
+//     {id: 1, hwName: "HW1", url: "https://forms.gle/MQejHVuzuVzj3uGE7"},
+//     {id: 2, hwName: "HW2", url: "https://forms.gle/6XVNWAyVc5ADfZxz9"},
+//     {id: 3, hwName: "HW3", url: "https://forms.gle/fhbZwTntgESBEY6j8"},
+//     {id: 4, hwName: "HW4", url: "https://forms.gle/2bRc93qwD8nwJafu5"},
+//     {id: 5, hwName: "Hw5", url: "https://forms.gle/65u4Er1kCaEMJioa7"}
+// ]
