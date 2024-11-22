f4a2ecb546126ca9f047120cc3d658eb9cb5bbee
Kira <kirasmle@gmail.com>
2024-11-22 17:51:52 -0500

    home tsx and css

--- adc/src/pages/home.tsx
+++ adc/src/pages/home.tsx
@@ -1,7 +1,34 @@
+import React from 'react';
+import './home.css';
+
 function Home() {
+    const handleStudentLogin = () => {
+        alert('Redirecting to Student Login');
+        // Add navigation or login logic here
+    };
+
+    const handleTeacherLogin = () => {
+        alert('Redirecting to Teacher Login');
+        // Add navigation or login logic here
+    };
+
     return (
-        <h1>Home</h1>
-    )
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1 id="title">C: USER\APPDEVCLUB\LOGIN: </h1>
+            <div style={{ marginTop: '20px' }}>
+                <button id="studentbut"
+                    onClick={handleStudentLogin} 
+                >
+                    Student Login
+                </button>
+                <button id="teacherbut"
+                    onClick={handleTeacherLogin} 
+                >
+                    Teacher Login
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
